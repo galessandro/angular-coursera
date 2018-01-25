7394ad1821ce261dec9cb9e2b571d4ab7cc19b44
Chambito <dimesandro@gmail.com>
2018-01-24 22:15:01 -0500

    quinta tarea de angular

---
+++ solution-5/src/public/news-items/news-items.controller.js
@@ -0,0 +1,15 @@
+(function(){
+
+"use strict";
+
+angular.module('public')
+.controller('NewsItemsController', NewsItemsController);
+
+//NewsItemsController.$inject = ['a'];
+function NewsItemsController(){
+	var $ctrl = this;
+
+
+}
+	
+})();
